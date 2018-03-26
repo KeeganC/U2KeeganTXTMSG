da5b7ed8f1d5d94d1222f52614777df55ed28f0e
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-03-26 11:40:47 -0400

    Add files via upload
    Added first files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿/*Keegan Chan
+ * 3 26 2018
+ * U2KeeganTXTMSG
+ * Translates leetspeak
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U2KeeganTXTMSG
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            string strInput = txbInput.Text;
+
+            strInput = strInput.Replace("CU", "see you").Replace(":-)", "I'm happy").Replace(":-(", "I'm unhappy");
+        }
+    }
+}
